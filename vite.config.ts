6321c88ac989dd77e7a3ce4b758036720f571f01
alfourqancenter <137097126+22110405@users.noreply.github.com>
2025-05-15 21:17:27 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Sci-Sphere/' : '/', // Replace 'repo-name' with your GitHub repository name
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist', // Default output directory for Vite
+    sourcemap: mode === 'development', // Optional: Enable sourcemaps only in development
+  },
+}));
